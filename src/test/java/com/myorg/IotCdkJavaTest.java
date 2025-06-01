21a4c43702c441da95c756e08284dbe4a80a9aa0
lakshmanlucky007 <lakshman.kumar@ilearnplace.com>
2025-06-01 08:53:16 +0530

    Initial commit

---
+++ src/test/java/com/myorg/IotCdkJavaTest.java
@@ -0,0 +1,26 @@
+// package com.myorg;
+
+// import software.amazon.awscdk.App;
+// import software.amazon.awscdk.assertions.Template;
+// import java.io.IOException;
+
+// import java.util.HashMap;
+
+// import org.junit.jupiter.api.Test;
+
+// example test. To run these tests, uncomment this file, along with the
+// example resource in java/src/main/java/com/myorg/IotCdkJavaStack.java
+// public class IotCdkJavaTest {
+
+//     @Test
+//     public void testStack() throws IOException {
+//         App app = new App();
+//         IotCdkJavaStack stack = new IotCdkJavaStack(app, "test");
+
+//         Template template = Template.fromStack(stack);
+
+//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+//           put("VisibilityTimeout", 300);
+//         }});
+//     }
+// }
